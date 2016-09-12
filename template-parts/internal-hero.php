43ee688001d3851105a101f867a79a839babb553
Roary Tubbs <roary.tubbs@gmail.com>
2016-09-12 14:52:56 -0500

    Updating header zoom

--- template-parts/internal-hero.php
+++ template-parts/internal-hero.php
@@ -5,8 +5,8 @@ $bg_position = get_post_meta( get_the_ID(), '_cf_bg_position', true);
 ?>
 <div class="hero" style="background-image:url(<?php the_post_thumbnail_url() ?>); background-position: <?php echo esc_html( $bg_position ); ?>;">
 	<span class="overlay"></span>
-	<div class="hero-content zoom">
-		<div class="page-container">
+	<div class="hero-content">
+		<div class="page-container zoom">
 			<h1 class="entry-title"><?php echo esc_html( $custom_title ); ?></h1>
 			<h2 class="subtitle"><?php echo esc_html( $custom_subtitle ); ?></h2>
 		</div><!-- page-container -->
